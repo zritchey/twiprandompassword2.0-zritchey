302f9ce7c31755ff2dcb9192767f6d6816b79311
zachary <zachrritchey@gmail.com>
2016-10-17 20:39:38 -0400

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,50 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            Scanner kb=new Scanner(System.in);
+            System.out.print("Choose your username: ");
+            String uName=kb.nextLine();
+            while (uName.indexOf(" ")>0){
+                System.out.print("Spaces aren't allowed in usernames.\nPlease enter a new username: ");
+                uName=kb.next();
+            }
+        System.out.println("Welcome "+uName+"!");
+        Password tired=new Password();
+        String password=tired.id();
+        System.out.println("Your password is: "+password);
+        System.out.println("\nWould you ike to log in?");
+        String ans=kb.next();
+        int login=0;
+
+        String check1="";
+        String check2="";
+        if (ans.trim().substring(0,1).equalsIgnoreCase("y"))
+            login++;
+        while(login==1){
+
+            System.out.print("Username: ");
+            while (check1.length()==0)
+             check1=kb.nextLine();
+
+
+            System.out.print("Password: ");
+            check2 = kb.nextLine().trim();
+
+
+            if(check1.equals(uName)&&check2.equals(password)){
+                login++;
+            }
+            else{
+                System.out.println("Your username or password is incorrect. ");
+            }
+        }
+         if(login==2){
+             System.out.println("Welcome to your account!");
+         }
+
     }
 }
---
+++ src/com/company/Password.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+/**
+ * Created by zachary on 10/17/2016.
+ */
+class Password {
+
+    private String generate;
+    private String pass;
+    private int a[]=new int[16];
+
+    public Password() {
+        generate = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+
+    }
+    private void numgen(){
+        int j = 0;
+        while (j < 16) {
+            a[j] = new Random().nextInt(70);
+            j++;
+        }
+    }
+    public String id(){
+        numgen();
+        int j=0;
+        pass="";
+        while (j<16){
+            pass=pass+generate.substring(a[j],a[j]+1);
+            j++;
+        }
+        return pass;
+    }
+
+
+}
